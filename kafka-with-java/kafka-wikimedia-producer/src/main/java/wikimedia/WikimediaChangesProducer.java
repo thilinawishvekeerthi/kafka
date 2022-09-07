0332a895c10d7c00ee2415ccd9bbc40dc930b75f
thilinawishvekeerthi <thilinawishvekeerthi@outlook.com>
2022-09-07 07:53:40 +0530

    first commit

---
+++ kafka-with-java/kafka-wikimedia-producer/src/main/java/wikimedia/WikimediaChangesProducer.java
@@ -0,0 +1,39 @@
+package wikimedia;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class WikimediaChangesProducer {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        String boostrapServers = "127.0.0.1:9092";
+
+        //create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create producer
+        KafkaProducer<String, String > producer = new KafkaProducer<String, String>(properties);
+        String topic = "wikimedia.recentchanges";
+
+        EventHandler eventHandler = new WikimediaEventSourceHandler(producer, topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource eventSource = builder.build();
+
+        eventSource.start();
+
+        TimeUnit.MINUTES.sleep(1);
+    }
+
+}
---
+++ kafka-with-java/kafka-wikimedia-producer/src/main/java/wikimedia/WikimediaEventSourceHandler.java
@@ -0,0 +1,46 @@
+package wikimedia;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WikimediaEventSourceHandler implements EventHandler {
+
+    KafkaProducer<String, String > producer;
+    String topic;
+    private final Logger log = LoggerFactory.getLogger(WikimediaEventSourceHandler.class.getName());
+
+    WikimediaEventSourceHandler(KafkaProducer<String, String > producer, String topic){
+        this.producer = producer;
+        this.topic = topic;
+    }
+
+    @Override
+    public void onOpen() throws Exception {
+
+    }
+
+    @Override
+    public void onClosed() throws Exception {
+        producer.close();
+    }
+
+    @Override
+    public void onMessage(String event, MessageEvent messageEvent) throws Exception {
+        log.info(messageEvent.getData());
+        producer.send(new ProducerRecord<>(topic, messageEvent.getData()));
+    }
+
+    @Override
+    public void onComment(String comment) throws Exception {
+
+    }
+
+    @Override
+    public void onError(Throwable t) {
+        log.error("ERROR IN WIKIMEDIA STREAM READING", t);
+    }
+}
